020b2e23c5ecec0ac9bb537f370d46fd00fb6e09
Oussama Anadani <anadani@student.chalmers.se>
2025-05-19 23:05:48 +0200

    added structrue and some todos for the fetcher.py

---
+++ src/hn_rag/fetcher.py
@@ -0,0 +1,6 @@
+HACKERNEWS_API = "https://hacker-news.firebaseio.com/v0"
+
+# TODO Fetch story IDs (/v0/topstories, /v0/newstories)
+# and pull full item JSONs
+
+# TODO Store raw JSON locally(or in a simple db)
