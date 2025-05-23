efce3eee6b21b61e970e616fc717d0555c8fea98
Oussama Anadani <anadani@student.chalmers.se>
2025-05-23 22:24:32 +0200

    added chroma vecDB

---
+++ src/vector_store.py
@@ -0,0 +1,11 @@
+from langchain_community.vectorstores import Chroma
+
+from embedder import get_embedding_model
+
+
+def build_vector_store(chunks: list) -> Chroma:
+    """
+    Create a Chroma vector store from document chunks using open-source embeddings.
+    """
+    embed_model = get_embedding_model()
+    return Chroma.from_documents(chunks, embed_model)
